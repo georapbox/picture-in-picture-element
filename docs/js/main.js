8faeca63d05920828993da73d142c920f53ba094
georapbox <georapbox@gmail.com>
2022-12-06 10:03:41 +0200

    Update demo

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,41 +1,90 @@
 const isLocalhost = window.location.href.includes('127.0.0.1') || window.location.href.includes('localhost');
 const componentUrl = isLocalhost ? '../../dist/picture-in-picture.js' : 'https://unpkg.com/@georapbox/picture-in-picture-element';
 
+const escapeHTML = subjectString => {
+  if (typeof subjectString !== 'string') {
+    return subjectString;
+  }
+
+  const htmlEscapes = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;' // eslint-disable-line quotes
+  };
+
+  const regexUnescapedHtml = /[&<>"'`]/g;
+  const regexHasUnescapedHtml = RegExp(regexUnescapedHtml.source);
+
+  return regexHasUnescapedHtml.test(subjectString)
+    ? subjectString.replace(regexUnescapedHtml, tag => htmlEscapes[tag] || tag)
+    : subjectString;
+};
+
+const customizeCheckbox = document.getElementById('customize');
+const htmlSrcEl = document.getElementById('html-source');
+
+const sourceTemplate = (slot = '', classAttr = '') => escapeHTML(`<picture-in-picture${classAttr}>${slot}</picture-in-picture>`);
+
+htmlSrcEl.innerHTML = sourceTemplate('', ' class="custom-styles"');
+
 import(componentUrl).then(res => {
   const { PictureInPicture } = res;
 
-  // const $console = document.getElementById('console');
-
   PictureInPicture.defineCustomElement();
 
-  document.getElementById('create').addEventListener('click', () => {
-    const pip = document.createElement('picture-in-picture');
+  const wc = document.querySelector('picture-in-picture');
 
-    const v = document.createElement('video');
-    v.src = 'assets/bigbuckbunny.mp4';
-    v.controls = true;
-    // v.disablePictureInPicture = true;
+  const renderElement = (isCustomized = true) => {
+    let slotTemplate = '';
+    const videoContent = '\n  <video src="assets/bigbuckbunny.mp4" controls></video>\n';
+    const slotContent = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/><path d="M8 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-3z"/></svg>\n    <span class="visually-hidden">Picture-In-Picture</span>`;
 
-    pip.appendChild(v);
+    if (isCustomized) {
+      slotTemplate = `${videoContent}  <span slot="pip-button-label">\n    ${slotContent}\n  </span>\n`;
 
-    document.getElementById('view').appendChild(pip);
-  });
+      const span = document.createElement('span');
+      span.slot = 'pip-button-label';
+      span.innerHTML = slotContent;
+      wc.appendChild(span);
+    } else {
+      slotTemplate = videoContent;
+      const slotEl = wc.querySelector('span[slot="pip-button-label"]');
+
+      if (slotEl) {
+        slotEl.remove();
+      }
+    }
+
+    wc.classList.toggle('custom-styles', isCustomized);
+
+    htmlSrcEl.innerHTML = sourceTemplate(slotTemplate, isCustomized ? ' class="custom-styles"' : '');
+
+    window.hljs.highlightElement(htmlSrcEl);
+  };
 
   document.addEventListener('picture-in-picture:enter', evt => {
-    console.log('pip enter', evt.detail);
-
-    evt.detail.pipButton.textContent = 'Exit Picture-In-Picture';
+    console.log('picture-in-picture:enter ->', evt.detail);
   });
 
   document.addEventListener('picture-in-picture:leave', evt => {
-    console.log('pip leave', evt.detail);
-
-    evt.detail.pipButton.textContent = 'Enter Picture-In-Picture';
+    console.log('picture-in-picture:leave ->', evt.detail);
   });
 
   document.addEventListener('picture-in-picture:error', evt => {
-    console.error('pip error', evt.detail.error);
+    console.log('picture-in-picture:error ->', evt.detail);
   });
+
+  document.addEventListener('picture-in-picture:resize', evt => {
+    console.log('picture-in-picture:resize ->', evt.detail);
+  });
+
+  customizeCheckbox.addEventListener('change', evt => {
+    renderElement(evt.target.checked);
+  });
+
+  renderElement(customizeCheckbox.checked);
 }).catch(err => {
   console.error(err);
 });
